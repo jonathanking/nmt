04b4408df1a5d935b8b4aa3c8ca8f2496fc6bcc7
Jonathan King <jonathanking@berkeley.edu>
2017-12-05 11:56:15 -0500

    Modified TF version check in order to run without using the most recent dev release of TF.

--- nmt/utils/misc_utils.py
+++ nmt/utils/misc_utils.py
@@ -29,7 +29,7 @@ import tensorflow as tf
 
 
 def check_tensorflow_version():
-  min_tf_version = "1.4.0-dev20171024"
+  min_tf_version = "1.4.0"
   if tf.__version__ < min_tf_version:
     raise EnvironmentError("Tensorflow version must >= %s" % min_tf_version)
 
@@ -134,21 +134,13 @@ def add_summary(summary_writer, global_step, tag, value):
   summary_writer.add_summary(summary, global_step)
 
 
-def get_config_proto(log_device_placement=False, allow_soft_placement=True,
-                     num_intra_threads=0, num_inter_threads=0):
+def get_config_proto(log_device_placement=False, allow_soft_placement=True):
   # GPU options:
   # https://www.tensorflow.org/versions/r0.10/how_tos/using_gpu/index.html
   config_proto = tf.ConfigProto(
       log_device_placement=log_device_placement,
       allow_soft_placement=allow_soft_placement)
   config_proto.gpu_options.allow_growth = True
-
-  # CPU threads options
-  if num_intra_threads:
-    config_proto.intra_op_parallelism_threads = num_intra_threads
-  if num_inter_threads:
-    config_proto.inter_op_parallelism_threads = num_inter_threads
-
   return config_proto
 
 
